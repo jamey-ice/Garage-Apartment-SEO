b9a04a01b58afbf861b3285b5d0aaec65df3d448
jamey-ice <jamey@6thavehomes.com>
2026-04-13 10:41:06 -0500

    Add Google Search Console verification meta tag

--- artifacts/dfw-garage-apartments/src/pages/_document.tsx
+++ artifacts/dfw-garage-apartments/src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="google-site-verification" content="FZql9nXIcieXF3le1YUetuQlhI3NEh_wmyE_VCiPQ7E" />
         <link rel="icon" href="/logos/logo-coral-navy.png" />
       </Head>
       <body>
